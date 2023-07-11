17fab726bd0d853ea74a288e68a5789d7b33c66e
AfyaNarh <nhyiraafya1@gmail.com>
2023-07-11 06:32:28 -0700

    if else

---
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Bestchool 98 Battery street")
+
+guillaume@ubuntu:~/0x01$ ./8-main.py
+BEST
+BESTCHOOL 98 BATTERYTREET
