97ac44af1f026f4ec3252b950be8a4ba984025a9
AfyaNarh <nhyiraafya1@gmail.com>
2023-07-18 01:55:42 +0000

    update

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,23 +1,14 @@
-#include <stdio.h>
 #include <Python.h>
 
-/**
- * print_python_list_info - prints python list info
- *
- * @p: PyObject
- * Return: no return
- */
 void print_python_list_info(PyObject *p)
 {
-	long int size, i;
-	PyListObject *list;
+	Py_ssize_t size, i;
 	PyObject *item;
 
-	size = Py_SIZE(p);
+	size = PyList_Size(p);
 	printf("[*] Size of the Python List = %ld\n", size);
 
-	list = (PyListObject *)p;
-	printf("[*] Allocated = %ld\n", list->allocated);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size; i++)
 	{
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
-    biggest = my_list[0]
+    if not my_list:
+        return None
+
+    max_num = my_list[0]
     for num in my_list:
-        if not my_list:
-            return None
-        if num > biggest:
-            biggest = num
-    return biggest
+        if num > max_num:
+            max_num = num
+
+    return max_num
