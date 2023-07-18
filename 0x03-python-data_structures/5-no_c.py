2449369fc5db97214156ffdbfed543742283181b
AfyaNarh <129697704+AfyaNarh@users.noreply.github.com>
2023-07-18 02:10:45 +0000

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def no_c(my_string):
-    mine = ""
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            mine = mine + i
-    return mine
--- 0x03-python-data_structures/README.md
+++
@@ -1,1 +0,0 @@
-Python - Datatructures: Lists, Tuples
