9361607d311b7d33cbb531a4ee2c42e323bbb461
AfyaNarh <129697704+AfyaNarh@users.noreply.github.com>
2023-07-18 01:50:09 +0000

    Delete 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
-    for row in matrix:
-        for b in row:
-            print("{:3d}".format(b), end="")
-        print()
